c7fc901579b5ab27e623fa42d6047dfba942742a
adi <@boomerang>
2023-12-29 15:26:43 -0600

    Testing MeepMeep

--- MeepMeep/src/main/java/com/example/meepmeep/MeepTest.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepTest.java
@@ -1,4 +1,27 @@
 package com.example.meepmeep;
 
+        import com.acmerobotics.roadrunner.geometry.Pose2d;
+        import com.noahbres.meepmeep.MeepMeep;
+        import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+        import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
 public class MeepTest {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(12, -60, Math.PI/2))
+                                .forward(30)
+                                .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
 }
